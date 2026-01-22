f6594d3929153d4ea087ac6191e05ba82abf627a
zak <zakaria.nait@mail.polimi.it>
2026-01-22 21:41:37 +0100

    sisterm ajs

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['vercel.com'],
+  },
+}
+
+module.exports = nextConfig

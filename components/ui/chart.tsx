317bc5242baa43c8f0b187cf700169b4b1fc308d
zak <zakaria.nait@mail.polimi.it>
2026-01-23 19:04:53 +0100

    s

--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -209,7 +209,7 @@ function ChartTooltipContent({
     >
       {!nestLabel ? tooltipLabel : null}
       <div className="grid gap-1.5">
-        {payload.map((item, index) => {
+        {payload.map((item: any, index: number) => {
           const key = `${nameKey || item.name || item.dataKey || 'value'}`
           const itemConfig = getPayloadConfigFromPayload(config, item, key)
           const indicatorColor = color || item.payload.fill || item.color
@@ -287,9 +287,10 @@ function ChartLegendContent({
   verticalAlign = 'bottom',
   nameKey,
 }: React.ComponentProps<'div'> &
-  Pick<RechartsPrimitive.LegendProps, 'payload' | 'verticalAlign'> & {
+  Omit<RechartsPrimitive.LegendProps, 'content' | 'formatter' | 'onBBoxUpdate'> & {
     hideIcon?: boolean
     nameKey?: string
+    payload?: any[]
   }) {
   const { config } = useChart()
 
